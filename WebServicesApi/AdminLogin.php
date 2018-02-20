ddf8ff57d02fe32ef9c7fd3af2699b72c017de16
Happy Singh <happysingh23828@gmail.com>
2018-02-21 00:55:26 +0530

    ....

--- WebServicesApi/AdminLogin.php
+++ WebServicesApi/AdminLogin.php
@@ -9,9 +9,20 @@
 	$result = $db->adminLogin($_POST['Email'],$_POST['Password']);
 	if($result==1)
 	{
-		$Admin = $db->getAdminDetailsByEmail($_POST['Email']);
+		$admin = $db->getAdminDetailsByEmail($_POST['Email']);
 		$response['error'] = false;
 		$response['message'] = " Login Successfuly....";
+		$response['name'] = $admin['name'];
+		$response['email'] = $admin['email'];
+		$response['collegecode'] = $admin['collegecode'];
+		$response['mobileno'] = $admin['mobileno'];
+		$response['dob'] = $admin['dob'];
+		$response['gender'] = $admin['gender'];
+		$response['profilephoto'] = $admin['profilephoto'];
+		$response['collegelogo'] = $admin['collegelogo'];
+		$response['collegename'] = $admin['collegename'];
+		$response['collegecity'] = $admin['collegecity'];
+		$response['collegestate'] = $admin['collegestate'];
 
 	}
 	else if($result==2)
